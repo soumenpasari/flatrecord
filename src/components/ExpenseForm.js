ffe83fb3698a52fea5aba98dae77c4692a374fb2
soumenpasari <soumenpasari849@gmail.com>
2020-07-14 20:19:42 +0530

    feat:added radio button

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,12 +3,14 @@ import React from 'react';
 // importing other components
 import {Textbar} from '../components/forms/Textbar';
 import { FormButton } from './forms/FormButton';
+import { RadioBtn } from './forms/RadioBtn';
 
 export const ExpenseForm = () => {
     return (
         <form>
             <Textbar placeholder='Expense title' flatIdName='TitleExp' type='text' />
             <Textbar placeholder='Amount' flatIdName='TitleExp' type='number' />
+            <RadioBtn />
             <FormButton title='Submit' type='submit' />
         </form>
     )
---
+++ src/components/forms/RadioBtn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import css module
+import 'bulma-checkradio/dist/css/bulma-checkradio.min.css';
+
+export const RadioBtn = () => {
+    return (
+        <div className='field'>
+            <input className='is-checkradio' type='radio' name='expenseType'
+             value='dr' id='expenseTypeDebit'/>
+            <label htmlFor='expenseTypeDebit'>Debit</label>
+            <input className='is-checkradio' type='radio' name='expenseType' value='dr' id='expenseTypeCredit' />
+            <label htmlFor='expenseTypeCredit'>Credit</label>
+        </div>
+    )
+}
